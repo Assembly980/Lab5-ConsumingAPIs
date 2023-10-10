be8a921ec4dfcc5c9bc31ea66eca40dceb8c98a5
Assembly980 <113926702+Assembly980@users.noreply.github.com>
2023-10-10 14:35:37 -0400

    Add files via upload

---
+++ CatResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Lab
+{
+    internal class CatResult
+    {
+        public string _id { get; set; }
+        public string user { get; set; }
+        public string text { get; set; }
+        public string type { get; set; }
+        public bool? deleted { get; set; }
+        public string createdAt { get; set; }
+        public string? updatedAt { get; set; }
+        public int __v { get; set; }
+
+
+        public CatResult() { }
+
+        public CatResult(string iD, int v, string text, string lastUpdate, bool deleted, string createdAt, string user, string type)
+        {
+            _id = iD;
+            __v = v;
+            this.user = user;
+            this.text = text;
+            this.type = type;
+            this.createdAt = createdAt;
+            this.updatedAt = lastUpdate;
+            this.deleted = deleted;
+        }
+
+        public override string ToString()
+        {
+            string msg = $"Fact: {text}\n";
+            return msg;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace API_Lab
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string response = " ";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("WARNING! \nTHE FOLLOWING API CAN BE ADDED TO BY ANYONE \nTHIS MEANS THAT SOME OF THE FACTS WILL BE INCOHERRENT" +
+                "\nBUT THIS SHOULD NOT DISSUADE YOU \nIF YOU RUN INTO AN INCOHERRENT FACT, PLEASE JUST HIT ENTER.");
+            Console.ResetColor();
+            do
+            {
+                await CatFactCall();
+                Console.WriteLine("Do you want another fact? (N to quit. Press any other key to continue)");
+                response = Console.ReadLine() + " ";
+            } while (response.ToUpper()[0] != 'N');
+        }
+
+        public static async Task CatFactCall()
+        {
+            var client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync("https://cat-fact.herokuapp.com/facts/random");
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+            
+            CatResult c = JsonSerializer.Deserialize<CatResult>(json, options);
+            Console.WriteLine(c + "\n");
+        }
+    }
+}
